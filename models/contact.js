7e99630c5c1d7802523c6a7433ce928268d96da5
Morifer <124445677+Morifer79@users.noreply.github.com>
2023-11-04 17:07:42 +0200

    fix some errors

--- models/contact.js
+++ models/contact.js
@@ -1,8 +1,8 @@
 const { Schema, model } = require("mongoose");
 const Joi = require("joi");
-const handleMongooseError = require("../helpers");
+const { handleMongooseError } = require("../helpers");
 
-const contactSchema = Schema(
+const contactSchema = new Schema(
   {
     name: {
       type: String,
