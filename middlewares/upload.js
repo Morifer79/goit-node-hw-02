25555092e71221c120111f08c18ffb3e4cf203dd
Hennadii Laktionov <morifer13@gmail.com>
2023-11-19 01:02:11 +0200

    complete task without test

---
+++ middlewares/upload.js
@@ -0,0 +1,17 @@
+import multer from 'multer';
+import path from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const tempDir = path.join(__dirname, '../', 'temp');
+
+const multerConfig = multer.diskStorage({
+  destination: tempDir,
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({
+  storage: multerConfig,
+});
