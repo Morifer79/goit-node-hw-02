71838743b7d28bacde9dc498a15743a94a8b8872
Hennadii Laktionov <morifer13@gmail.com>
2023-11-21 17:11:24 +0200

    add supertest

--- login.test.js
+++ login.test.js
@@ -1,13 +1,43 @@
 /*
-
 Response must have status 200
 Response must return the token
 Response must return object user with 2 fields: email and subscription with type of data String
-
 */
 
-describe('test login route', async () => {
-  const resp = await request(app).post('/users/login');
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import request from 'supertest';
+import app from './app';
+
+dotenv.config();
+mongoose.set('strictQuery', true);
+
+const { DB_HOST, PORT } = process.env;
+
+const exampleLogin = {
+  email: 'eva@mail.com',
+  password: '123456',
+};
+
+describe('test login controller', () => {
+  beforeAll(async () => {
+    try {
+      await mongoose.connect(DB_HOST);
+      app.listen(PORT);
+    } catch (error) {
+      process.exit(1);
+    }
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
+  let resp;
+
+  beforeEach(async () => {
+    resp = await request(app).post('/users/login').send(exampleLogin);
+  });
 
   is('status - 200', async () => {
     expect(resp.status).toBe(200);
